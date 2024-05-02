c9772c8f56ef9ac03f0ce3c34c934d1d89a278c9
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-05-02 15:47:32 +0100

    Add visualisations to contexts - Update query params with default values on page load - Rejig the appConfig to allow for multiple visualisations affected by a single filter

--- mapReducer.js
+++ mapReducer.js
@@ -7,7 +7,8 @@ export const actionTypes = {
     ADD_LAYER: 'ADD_LAYER',
     ADD_PARAMETERISED_LAYER: 'ADD_PARAMETERISED_LAYER',
     UPDATE_PARAMETERISED_LAYER: 'UPDATE_PARAMETERISED_LAYER',
-    SET_FILTERS: 'SET_FILTERS',
+    ADD_VISUALISATION: 'ADD_VISUALISATION',
+    UPDATE_QUERY_PARAMS: 'UPDATE_QUERY_PARAMS',
     FETCH_DATA: 'FETCH_DATA',
     JOIN_DATA: 'JOIN_DATA',
     SET_IS_LOADING: 'SET_IS_LOADING'
@@ -16,7 +17,7 @@ export const actionTypes = {
 export const mapReducer = (state, action) => {
     switch (action.type) {
         case actionTypes.RESET_CONTEXT:
-            return { ...state, layers: [] };
+            return { ...state, layers: {}, visualisations: {} };
         case actionTypes.SET_PAGE_INFO:
             return { ...state, pageInfo: action.payload} 
         case actionTypes.INITIALISE_SIDEBAR:
@@ -24,7 +25,7 @@ export const mapReducer = (state, action) => {
         case actionTypes.ADD_LAYER:
             // Logic to add a non-parameterised layer
             return { ...state, layers: { ...state.layers, ...action.payload } };
-        case actionTypes.UPDATE_PARAMETERISED_LAYER:
+        case actionTypes.UPDATE_PARAMETERISED_LAYER: {
             // Get the layer name and new parameters from the payload
             const layerName = action.payload.filter.layer;
             const paramName = action.payload.filter.paramName;
@@ -35,16 +36,43 @@ export const mapReducer = (state, action) => {
             return {
                 ...state,
                 layers: {
-                ...state.layers,
-                [layerName]: {
-                    ...state.layers[layerName],
-                    path: updatedPath
-                }
+                    ...state.layers,
+                    [layerName]: {
+                        ...state.layers[layerName],
+                        path: updatedPath
+                    }
                 }
             };
-        case actionTypes.SET_FILTERS:
-            // Logic to set filters
-            break;
+        }
+        case actionTypes.ADD_VISUALISATION: {
+            // Logic to add a visualisation
+            return { ...state, visualisations: { ...state.visualisations, ...action.payload } };
+        }
+        case actionTypes.UPDATE_QUERY_PARAMS: {
+            const visualisationNames = action.payload.filter.visualisations;
+            const paramName = action.payload.filter.paramName;
+            const newParamValue = action.payload.value;
+
+            // Create a new visualisations object with updated query params for each visualisation
+            const updatedVisualisations = { ...state.visualisations };
+            visualisationNames.forEach((visName) => {
+                if (updatedVisualisations[visName]) {
+                    updatedVisualisations[visName] = {
+                        ...updatedVisualisations[visName],
+                        queryParams: {
+                        ...updatedVisualisations[visName].queryParams,
+                        [paramName]: newParamValue,
+                        },
+                    };
+                }
+            });
+
+            // Return the new state with updated visualisations
+            return {
+                ...state,
+                visualisations: updatedVisualisations,
+            };
+        }
         case actionTypes.FETCH_DATA:
             // Logic to fetch data
             break;
@@ -59,49 +87,3 @@ export const mapReducer = (state, action) => {
             return state;
     }
 };
-
-
-// const addLayerFromGeoJson = async (map, layers) => {
-//     const fetchLayer = async (layer) => {
-//       const geojson = await api.geodataService.getLayer(layer)
-//       return { id: layer.name, geojson: geojson, geometryType: layer.geometryType };
-//     };
-  
-//     Promise.all(layers.map(fetchLayer))
-//       .then((geojsonLayers) => {
-//         geojsonLayers.forEach(({ id, geojson, geometryType }) => {
-//           if (map.getSource(id)) {
-//             return;
-//           }
-  
-//           map.addSource(id, { type: 'geojson', data: geojson });
-//           switch (geometryType) {
-//             case 'line':
-//               map.addLayer({
-//                 'id': id,
-//                 'type': 'line',
-//                 'source': id,
-//                 'paint': {
-//                   'line-color': 'black',
-//                   'line-opacity': 0.8
-//                 }
-//               });
-//               break;
-//             case 'polygon':
-//               map.addLayer({
-//                 'id': id,
-//                 'type': 'fill',
-//                 'source': id,
-//                 'paint': {
-//                   'fill-color': 'black',
-//                   'fill-opacity': 0.8
-//                 }
-//               });
-//               break;
-//             default:
-//               break;
-//           }
-//         });
-//       })
-//       .catch((error) => console.error('Error fetching GeoJSON:', error));
-//   };
