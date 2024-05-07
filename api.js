ec92b56f439ade15ba3f117958e5aa4f7855a019
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-05-07 18:08:03 +0100

    Improve handling of parameterised API URLs Mitigate false positives when vector tiles retrieved

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
-export const hasRouteParameter = (path) => /\{.+?\}/.test(path);
+export const hasRouteParameter = (path) => {
+    // Check if the path contains at least one "{parameter}" other than "{z}/{x}/{y}"
+    const parameterPattern = /\{(?![zyx]\/[^{}]*\})[^{}]+\}/; // Matches any parameter that is not {z}, {x}, or {y}
+    return parameterPattern.test(path);
+};
 
 export const replaceRouteParameter = (path, paramName, paramValue) => path.replace(`{${paramName}}`, paramValue);
 
