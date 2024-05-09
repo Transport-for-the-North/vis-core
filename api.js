d8b665f7c5f74420ab06386094a7adace8ff4dcd
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-05-09 23:49:38 +0100

    Add line-continuous styling, demonstrate with NoHAM VectorTile

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 export const hasRouteParameter = (path) => {
     // Check if the path contains at least one "{parameter}" other than "{z}/{x}/{y}"
-    const parameterPattern = /\{(?![zyx]\/[^{}]*\})[^{}]+\}/; // Matches any parameter that is not {z}, {x}, or {y}
+    const parameterPattern = /\{(?![zyx]\})[^{}]+\}/; // Matches any parameter that is not {z}, {x}, or {y}
     return parameterPattern.test(path);
 };
 
