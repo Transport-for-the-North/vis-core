b1b71699b4ce161b134ab617f4801684189e87fc
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-05-13 22:56:25 +0100

    Decouple sidebar from map context - Adds the initialisation logic to the MapLayout - Allows additional children to be rendered within the sidebar (for example, additional AccordionSections)

--- MapLayout/MapLayout.jsx
+++ MapLayout/MapLayout.jsx
@@ -1,8 +1,10 @@
+import { useContext, useEffect, useRef } from 'react'
 import styled from 'styled-components';
+
 import Map from "./Map";
 import { Sidebar } from 'Components';
-
-
+import { PageContext, MapContext } from 'contexts';
+import { loremIpsum } from 'utils';
 
 const LayoutContainer = styled.div`
   display: flex;
@@ -14,12 +16,47 @@ const MapContainer = styled.div`
   position: relative;
 `;
 
-
 export const MapLayout = () => {
+  const { state, dispatch } = useContext(MapContext);
+  const pageContext = useContext(PageContext);
+  const initializedRef = useRef(false);
 
+  useEffect(() => {
+    // Effect to initialise the filters for the map page
+    if (!initializedRef.current && Object.keys(state.visualisations).length > 0) {
+      pageContext.config.filters.forEach((filter) => {
+        if (filter.action === "UPDATE_QUERY_PARAMS") {
+          let defaultValue = filter.defaultValue || filter.min || filter.values?.values[0]?.paramValue;
+          dispatch({
+            type: filter.action,
+            payload: { filter, value: defaultValue },
+          });
+        }
+      });
+      initializedRef.current = true;
+    }
+  }, [pageContext.config.filters, dispatch, state.visualisations]);
+
+  useEffect(() => {
+    initializedRef.current = false;
+  }, [pageContext]);
+
+  const handleFilterChange = (filter, value) => {
+    dispatch({
+      type: filter.action,
+      payload: { filter, value },
+    });
+  };
+  
   return (
     <LayoutContainer>
-      <Sidebar />
+      <Sidebar 
+        pageName={pageContext.pageName}
+        aboutVisualisationText={loremIpsum}
+        filters={pageContext.config.filters}
+        legalText={loremIpsum}
+        onFilterChange={handleFilterChange}
+      />
       <MapContainer>
         <Map/>
       </MapContainer>
--- Sidebar/Sidebar.jsx
+++ Sidebar/Sidebar.jsx
@@ -1,48 +1,8 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-import { PageContext, AppContext } from "contexts";
-import { useMapContext } from "hooks";
 import { TextSection } from "./Accordion";
 import { SelectorSection } from "./Selectors";
 
-const LayerControlEntry = ({ layer }) => {
-  const { state } = useMapContext();
-  const { map } = state;
-  const [visibility, setVisibility] = useState(layer.layout.visibility);
-  const [opacity, setOpacity] = useState(layer.paint["fill-opacity"] || 1);
-
-  const toggleVisibility = () => {
-    const newVisibility = visibility === "visible" ? "none" : "visible";
-    map.setLayoutProperty(layer.id, "visibility", newVisibility);
-    setVisibility(newVisibility);
-  };
-
-  const handleOpacityChange = (e) => {
-    const newOpacity = parseFloat(e.target.value);
-    map.setPaintProperty(layer.id, "fill-opacity", newOpacity);
-    setOpacity(newOpacity);
-  };
-
-  return (
-    <div>
-      <label>{layer.id}</label>
-      <input
-        type="checkbox"
-        checked={visibility === "visible"}
-        onChange={toggleVisibility}
-      />
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.1"
-        value={opacity}
-        onChange={handleOpacityChange}
-      />
-    </div>
-  );
-};
 
 // Styled components for the sidebar
 const SidebarHeader = styled.h2`
@@ -73,97 +33,28 @@ const SidebarContainer = styled.div`
   scrollbar-color: rgba(0, 0, 0, 0.2) transparent;
 `;
 
-export const Sidebar = () => {
-  const { state, dispatch } = useMapContext();
-  const pageContext = useContext(PageContext);
-  const appContext = useContext(AppContext);
-  const initializedRef = useRef(false); // Ref to track if initialisation has occurred
-  // Destructure visualisations from state
-  const { visualisations, map } = state;
-
-  const [layers, setLayers] = useState([]);
-
-  useEffect(() => {
-    if (map) {
-      const updateLayers = () => {
-        const newLayers = map.getStyle().layers;
-        setLayers(
-          newLayers.filter(
-            (layer) =>
-              layer.type === "fill" ||
-              layer.type === "line" ||
-              layer.type === "circle"
-          )
-        );
-      };
-
-      // Update layers on initial load
-      updateLayers();
-
-      // Update layers whenever the map style changes
-      map.on("styledata", updateLayers);
-
-      // Clean up event listener when component unmounts
-      return () => {
-        map.off("styledata", updateLayers);
-      };
-    }
-  }, [map]);
-
-  useEffect(() => {
-    // Check if visualisations are ready and initialisation has not occurred yet
-    if (!initializedRef.current && Object.keys(visualisations).length > 0) {
-      // Initialize query params for each filter with a defaultValue
-      pageContext.config.filters.forEach((filter) => {
-        if (filter.action === "UPDATE_QUERY_PARAMS") {
-          let defaultValue;
-          if (filter.type === "dropdown") {
-            defaultValue = filter.values?.values[0]?.paramValue;
-          } else if (filter.type === "slider") {
-            defaultValue = filter.min;
-          }
-
-          if (defaultValue !== undefined) {
-            dispatch({
-              type: filter.action,
-              payload: { filter, value: defaultValue },
-            });
-          }
-        }
-      });
-      // Mark initialisation as done
-      initializedRef.current = true;
-    }
-  }, [pageContext.config.filters, dispatch, visualisations]);
-
-  useEffect(() => {
-    // Effect to reset initialised when pageContext changes.
-    initializedRef.current = false;
-  }, [pageContext]);
-
-  const handleFilterChange = (filter, value) => {
-    dispatch({
-      type: filter.action,
-      payload: { filter, value },
-    });
-  };
-
+export const Sidebar = ({
+  pageName,
+  aboutVisualisationText,
+  filters,
+  legalText,
+  onFilterChange,
+  children, // Accept children props
+}) => {
   return (
-    <SidebarContainer key={pageContext.pageName}>
+    <SidebarContainer>
       <SidebarHeader>
-        {pageContext.pageName || "Visualisation Framework"}
+        {pageName || "Visualisation Framework"}
       </SidebarHeader>
-      <TextSection title="About this visualisation" text="Placeholder" />
-      <SelectorSection
-        filters={pageContext.config.filters}
-        onFilterChange={handleFilterChange}
-      />
-      {/* <AccordionSection title="Layer Control">
-        {layers.map((layer) => (
-          <LayerControlEntry key={layer.id} layer={layer} />
-        ))}
-      </AccordionSection> */}
-      <TextSection title="Legal" text={appContext.legalText} />
+      <TextSection title="About this visualisation" text={aboutVisualisationText} />
+      {filters && (
+        <SelectorSection
+          filters={filters}
+          onFilterChange={(filter, value) => onFilterChange(filter, value)}
+        />
+      )}
+      {children} {/* Render additional AccordionSections passed as children */}
+      <TextSection title="Legal" text={legalText} />
     </SidebarContainer>
   );
 };
