a0b07ee33679cca42edd762a430634cf6aa40995
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-05-20 21:40:42 +0100

    Add switchable hover effect on maps

--- MapLayout/Map.jsx
+++ MapLayout/Map.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useCallback, useRef, useContext } from "react";
 import styled from "styled-components";
 import "maplibre-gl/dist/maplibre-gl.css";
-
-import { api } from 'services';
-import { useMap, useMapContext } from 'hooks';
-import { Visualisation } from './Visualisation'
+import { api } from "services";
+import { useMap, useMapContext } from "hooks";
+import { Visualisation } from "./Visualisation";
 import { PageContext } from "contexts";
 
 const StyledMapContainer = styled.div`
@@ -12,13 +11,23 @@ const StyledMapContainer = styled.div`
   height: calc(100vh - 75px);
 `;
 
-//{mapData, colourScheme}
+/**
+ * Map component that renders a map using MapLibre GL and handles layers,
+ * including hover and click interactions.
+ */
 const Map = () => {
   const mapContainerRef = useRef(null);
   const { map, isMapReady } = useMap(mapContainerRef);
-  const pageContext = useContext(PageContext)
+  const pageContext = useContext(PageContext);
   const { state, dispatch } = useMapContext();
-  
+
+  /**
+   * Generates the style configuration for a regular layer based on the geometry
+   * type of the layer.
+   *
+   * @param {string} geometryType - The type of geometry for the layer. Possible values are "polygon", "line", or "point".
+   * @returns {Object} The style configuration object for the layer.
+   */
   const getLayerStyle = (geometryType) => {
     switch (geometryType) {
       case "polygon":
@@ -56,24 +65,93 @@ const Map = () => {
     }
   };
 
+  /**
+   * Generates the style configuration for a hover layer based on the geometry
+   * type of the layer.
+   *
+   * @param {string} geometryType - The type of geometry for the hover layer. Possible values are "polygon", "line", or "point".
+   * @returns {Object} The style configuration object for the hover layer.
+   */
+  const getHoverLayerStyle = (geometryType) => {
+    switch (geometryType) {
+      case "polygon":
+        return {
+          id: "",
+          type: "line",
+          paint: {
+            "line-color": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              "rgba(255, 255, 255, 1.0)", // Hover color
+              "rgba(0, 0, 0, 1.0)", // Default color
+            ],
+            "line-width": 6,
+          },
+          filter: ["==", "id", ""],
+        };
+      case "line":
+        return {
+          id: "",
+          type: "line",
+          paint: {
+            "line-color": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              "rgba(255, 255, 255, 1.0)", // Hover color
+              "rgba(0, 0, 0, 1.0)", // Default color
+            ],
+            "line-opacity": 0.8,
+          },
+          filter: ["==", "id", ""],
+        };
+      case "point":
+        return {
+          id: "",
+          type: "circle",
+          paint: {
+            "circle-radius": 5,
+            "circle-color": [
+              "case",
+              ["boolean", ["feature-state", "hover"], false],
+              "rgba(255, 255, 255, 1.0)", // Hover color
+              "rgba(0, 0, 0, 1.0)", // Default color
+            ],
+          },
+          filter: ["==", "id", ""],
+        };
+      default:
+        return {};
+    }
+  };
+
+  /**
+   * Adds a new layer to the map based on the provided layer configuration.
+   * Handles both GeoJSON and tile layers and optionally adds a hover layer
+   * if the layer is marked as hoverable.
+   *
+   * @param {Object} layer - The layer configuration object containing information about the layer to be added to the map.
+   */
   const addLayerToMap = useCallback(
     (layer) => {
       if (!map.getSource(layer.name)) {
         let sourceConfig = {};
         let layerConfig = getLayerStyle(layer.geometryType);
         layerConfig.id = layer.name;
-        layerConfig.visibility = 'visible'
+        layerConfig.visibility = "visible";
 
         if (layer.type === "geojson") {
-          // Fetch and add a GeoJSON layer
           api.geodataService.getLayer(layer).then((geojson) => {
             sourceConfig.type = "geojson";
             sourceConfig.data = geojson;
             map.addSource(layer.name, sourceConfig);
-            map.addLayer({ ...layerConfig, source: layer.name});
+            map.addLayer({ ...layerConfig, source: layer.name });
+            if (layer.isHoverable) {
+              const hoverLayerConfig = getHoverLayerStyle(layer.geometryType);
+              hoverLayerConfig.id = `${layer.name}-hover`;
+              map.addLayer({ ...hoverLayerConfig, source: layer.name });
+            }
           });
         } else if (layer.type === "tile") {
-          // Add a tile layer
           const url =
             layer.source === "api"
               ? api.geodataService.buildTileLayerUrl(layer.path)
@@ -85,83 +163,158 @@ const Map = () => {
           map.addLayer({
             ...layerConfig,
             source: layer.name,
-            "source-layer": layer.sourceLayer
+            "source-layer": layer.sourceLayer,
           });
+
+          if (layer.isHoverable) {
+            const hoverLayerConfig = getHoverLayerStyle(layer.geometryType);
+            hoverLayerConfig.id = `${layer.name}-hover`;
+            hoverLayerConfig.source = layer.name;
+            hoverLayerConfig["source-layer"] = layer.sourceLayer;
+            map.addLayer(hoverLayerConfig);
+          }
         }
       }
     },
     [map]
   );
 
-  
-  // Function to handle map click events
-  const handleMapClick = useCallback((event) => {
-    if (!isMapReady || !map) return;
+  /**
+   * Handles hover events for a specific layer by setting the hover state
+   * for features under the mouse pointer.
+   *
+   * @param {Object} e - The event object containing information about the hover event.
+   * @param {string} layerId - The ID of the layer being hovered over.
+   */
+  const handleLayerHover = useCallback((e, layerId) => {
+    if (!map || !e.point) return;
 
-    // Get the point where the map is clicked
-    const point = event.point;
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: [layerId],
+    });
 
-    // Get all map filters
-    const mapFilters = pageContext.config.filters.filter(filter => filter.type === 'map');
+    if (features.length > 0) {
+      const feature = features[0];
+      map.setFilter(`${layerId}-hover`, ["==", "id", feature.id]);
+    }
+  }, [map]);
 
-    // For each map filter, check if the clicked point has a feature from the layer
-    mapFilters.forEach(filter => {
-      const features = map.queryRenderedFeatures(point, { layers: [filter.layer] });
-      if (features.length > 0) {
-        // Assuming the first feature is the one we're interested in
-        const feature = features[0];
-        const value = feature.properties[filter.field];
-        console.log(`Updating ${filter.field}`)
-        // Dispatch the action with the value from the clicked feature
-        dispatch({
-          type: filter.action,
-          payload: { filter, value }
-        });
+  /**
+   * Handles mouse leave events for a specific layer by clearing the hover state.
+   *
+   * @param {string} layerId - The ID of the layer from which the mouse has left.
+   */
+  const handleLayerLeave = useCallback((layerId) => {
+    if (!map) return;
+    map.setFilter(`${layerId}-hover`, ["==", "id", ""]);
+  }, [map]);
+
+  /**
+   * Effect to add hover and leave event listeners to hoverable layers.
+   */
+  useEffect(() => {
+    if (!map) return;
+
+    Object.keys(state.layers).forEach((layerId) => {
+      if (state.layers[layerId].isHoverable) {
+        map.on("mousemove", layerId, (e) => handleLayerHover(e, layerId));
+        map.on("mouseleave", layerId, () => handleLayerLeave(layerId));
       }
     });
-  }, [isMapReady, map, pageContext.config.filters, dispatch]);
-  
-  // Run once to set the state of the map
+
+    return () => {
+      Object.keys(state.layers).forEach((layerId) => {
+        if (state.layers[layerId].isHoverable) {
+          map.off("mousemove", layerId, (e) => handleLayerHover(e, layerId));
+          map.off("mouseleave", layerId, () => handleLayerLeave(layerId));
+        }
+      });
+    };
+  }, [map, handleLayerHover, handleLayerLeave, state.layers]);
+
+  /**
+   * Handles map click events and dispatches actions based on the clicked feature.
+   *
+   * @param {Object} event - The event object containing information about the click event.
+   */
+  const handleMapClick = useCallback(
+    (event) => {
+      if (!isMapReady || !map) return;
+
+      const point = event.point;
+
+      const mapFilters = pageContext.config.filters.filter(
+        (filter) => filter.type === "map"
+      );
+
+      mapFilters.forEach((filter) => {
+        const features = map.queryRenderedFeatures(point, {
+          layers: [filter.layer],
+        });
+        if (features.length > 0) {
+          const feature = features[0];
+          const value = feature.properties[filter.field];
+          console.log(`Updating ${filter.field}`);
+          dispatch({
+            type: filter.action,
+            payload: { filter, value },
+          });
+        }
+      });
+    },
+    [isMapReady, map, pageContext.config.filters, dispatch]
+  );
+
+  /**
+   * Effect to set the state of the map when it becomes ready.
+   */
   useEffect(() => {
     if (isMapReady) {
       dispatch({
-        type: 'SET_MAP',
+        type: "SET_MAP",
         payload: { map },
       });
     }
-  }, [isMapReady, map, dispatch]);
-  
-  // Set up the click event listener
+  }, [isMapReady, map]);
+
+  /**
+   * Effect to add click event listener to the map if there are map filters.
+   */
   useEffect(() => {
     if (isMapReady) {
-      // Check if there is a map filter before adding the click handler
-      const hasMapFilter = pageContext.config.filters.some(filter => filter.type === 'map');
+      const hasMapFilter = pageContext.config.filters.some(
+        (filter) => filter.type === "map"
+      );
       if (hasMapFilter) {
-        map.on('click', handleMapClick);
+        map.on("click", handleMapClick);
       }
     }
 
-    // Remove the click event listener when the component unmounts or dependencies change
     return () => {
       if (map) {
-        map.off('click', handleMapClick);
+        map.off("click", handleMapClick);
       }
     };
   }, [isMapReady, map, handleMapClick]);
 
+  /**
+   * Effect to add layers to the map and clean up on component unmount.
+   */
   useEffect(() => {
     if (isMapReady) {
       console.log("Map load within Map component");
       Object.values(state.layers).forEach((layer) => addLayerToMap(layer));
     }
 
-    // Remove layers when the component unmounts or layers change
     return () => {
       console.log("Map unmount");
       if (map) {
         Object.values(state.layers).forEach((layer) => {
           if (map.getLayer(layer.name)) {
             map.removeLayer(layer.name);
+          }
+          if (map.getLayer(`${layer.name}-hover`)) {
+            map.removeLayer(`${layer.name}-hover`);
           }
           if (map.getSource(layer.name)) {
             map.removeSource(layer.name);
@@ -174,7 +327,11 @@ const Map = () => {
   return (
     <StyledMapContainer ref={mapContainerRef}>
       {Object.values(state.visualisations).map((visConfig) => (
-        <Visualisation key={visConfig.name} visualisationName={visConfig.name} map={map} />
+        <Visualisation
+          key={visConfig.name}
+          visualisationName={visConfig.name}
+          map={map}
+        />
       ))}
     </StyledMapContainer>
   );
