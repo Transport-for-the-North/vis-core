910d996afe3a00842403bcddcd16d36fcb60c520
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-05-21 10:58:38 +0100

    Add assorted utilities including map opacity prop helper

--- utils/state.js
+++ utils/state.js
@@ -1,4 +1,4 @@
-const combineReducers = (reducers) => {
+const combineReducersWithAccessor = (reducers) => {
   return (state, action) => {
     return Object.keys(reducers).reduce((acc, prop) => {
       return {
@@ -8,6 +8,16 @@ const combineReducers = (reducers) => {
     }, state);
   };
 };
+
+function combineReducers(reducers) {  
+  return (state = {}, action) => {
+    const newState = {};
+    for (let key in reducers) {
+      newState[key] = reducers[key](state[key], action);
+    }
+    return newState;
+  }
+}
 
 const createActions = (namespace, actions) => {
   return Object.fromEntries(
