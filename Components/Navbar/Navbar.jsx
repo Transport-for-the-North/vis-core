f60593a7b4c8fe9083adac5f66ab3be49490a742
LTendron <ltendron1@systra.com>
2024-05-28 16:32:33 +0100

    Added a logout button

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { AppContext } from "contexts";
 import { Button } from "./Button";
 import { LateralNavbar } from "./LateralNavbar";
 import { Logo } from "./Logo";
-import "./Navbar.styles.css"
+import "./Navbar.styles.css";
 
 const StyledNavbar = styled.nav`
   display: flex;
@@ -17,17 +17,30 @@ const StyledNavbar = styled.nav`
   background-color: #f8f9fa; // Example background color, adjust as needed
 `;
 
+const StyledLogout = styled.img`
+  cursor: pointer;
+
+  @media only screen and (min-width: 766px) {
+    position: absolute;
+    right: 1%;
+  }
+
+  @media only screen and (max-width: 765px) {
+    display: none;
+  }
+`;
+
 export function Navbar() {
-  const location = useLocation()
+  const location = useLocation();
   const [isClicked, setIsClicked] = useState(false);
   const [activeLink, setActiveLink] = useState("");
   const [sideNavOpen, setSideNavOpen] = useState("sideNavbar-notShown");
   const appContext = useContext(AppContext);
   const navigate = useNavigate();
 
-  const onClick = (url) => { 
+  const onClick = (url) => {
     navigate(url);
-  }
+  };
 
   const updateMenu = () => {
     if (!isClicked) {
@@ -38,33 +51,39 @@ export function Navbar() {
     setIsClicked(!isClicked);
   };
 
-  const handleClick = () => {
-    setSideNavOpen("sideNavbar-notShown");
-    setIsClicked(!isClicked);
-  };
+  const handleLogout = () => {
+    //TODO : Add the logout behavior
+  }
 
   useEffect(() => {
-    setActiveLink(location.pathname)
+    setActiveLink(location.pathname);
     if (sideNavOpen === "sideNavbar-shown") updateMenu();
   }, [location]);
 
   return (
     <>
       <StyledNavbar className="navbar">
-        <Logo className="logoNav" onClick={() => onClick("/")}/>
-        <LateralNavbar className={sideNavOpen} handleClick={handleClick} />
+        <Logo className="logoNav" onClick={() => onClick("/")} />
+        <LateralNavbar className={sideNavOpen} onClick={() => handleLogout()} />
         {appContext.appPages.map((page) => (
-            <Link key={page.pageName} className={activeLink===page.url ? "ActiveNavButton" : "NavButton"} to={page.url} >
-              {page.pageName}
-            </Link>
+          <Link
+            key={page.pageName}
+            className={
+              activeLink === page.url ? "ActiveNavButton" : "NavButton"
+            }
+            to={page.url}
+          >
+            {page.pageName}
+          </Link>
         ))}
-        
+
         <Button
           className="navbarMobile"
           src="/img/burgerIcon.png"
           alt="Burger Button Navbar"
           onClick={updateMenu}
         />
+        <StyledLogout src="/img/logout.png" onClick={() => handleLogout}/>
       </StyledNavbar>
       <div className="empty-blank-nav"></div>
     </>
