7f154f6aa44058d4f8f030108869eec989c50e3d
Adon P M <apm1@systra.com>
2024-07-09 12:08:52 +0530

    Updated Alistair comments

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
 import './index.css';
 import App from './App';
 import { Auth0ProviderWithHistory } from 'contexts';
@@ -18,21 +16,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const isDev = process.env.REACT_APP_NAME === 'dev';
 
 root.render(
-  <Provider store={store}>
-    <Router>
-      {isDev ? (
+  <Router>
+    {isDev ? (
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    ) : (
+      <Auth0ProviderWithHistory>
         <React.StrictMode>
           <App />
         </React.StrictMode>
-      ) : (
-        <Auth0ProviderWithHistory>
-          <React.StrictMode>
-            <App />
-          </React.StrictMode>
-        </Auth0ProviderWithHistory>
-      )}
-    </Router>
-  </Provider>
+      </Auth0ProviderWithHistory>
+    )}
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
