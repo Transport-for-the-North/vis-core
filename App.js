3927401b56f7b481360414eb9a3bb45021d8dd18
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-07-16 14:17:59 +0100

    orModify workflow to alter metadata of the index page

--- App.js
+++ App.js
@@ -39,8 +39,6 @@ function App() {
           apiSchema: apiSchema,
         });
 
-        document.title = initialAppConfig.title
-
       } catch (error) {
         console.error('Failed to load app configuration:', error);
       }
