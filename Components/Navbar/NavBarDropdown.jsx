68cae0cd4517ff501b31405b9ca3b3715fe54a6b
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-07-18 12:40:44 +0100

    Minor styling changes to navbar - Alignment of dropdowns - Padding on dropdown items - Dropdown icon - Hover effect

--- Components/Navbar/NavBarDropdown.jsx
+++ Components/Navbar/NavBarDropdown.jsx
@@ -8,14 +8,21 @@ const DropdownContainer = styled.div`
   display: inline-block;
   font-family: var(--standardFontFamily);
   font-size: larger;
-  text-decoration: 0;
+  text-decoration: none;
   width: 12%;
   max-width: 270px;
   text-align: center;
   cursor: pointer;
+  padding: 0 5px;
+  height: 100%; /* Full height of navbar */
+  display: flex;
+  align-items: center; /* Vertically centre text */
+  justify-content: center; /* Centre text horizontally */
+  border-bottom-right-radius: 65px;
 
   @media only screen and (max-width: 1165px) {
     font-size: large;
+    border-bottom-right-radius: 30px;
   }
 
   @media only screen and (max-width: 930px) {
@@ -30,24 +37,28 @@ const DropdownContainer = styled.div`
 const DropdownMenu = styled.div`
   display: ${(props) => (props.open ? "block" : "none")};
   position: absolute;
+  left: 0; /* Align to the right of the dropdown item */
+  top: 75px; /* Position dropdown at the bottom of the navbar */
   background-color: #f9f9f9;
   min-width: 160px;
   width: 100%;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
-  border-radius: 2px;
+  border-radius: 0 0 5px 5px;
+  overflow: hidden;
 `;
 
 const DropdownItem = styled(Link)`
   width: 100%;
-  padding: 12px 0px;
+  padding: 12px 10px; /* Adjusted padding */
   text-decoration: none;
   display: block;
   font-size: large;
   border-radius: 2px;
   text-align: left;
+  box-sizing: border-box; /* Ensure padding is inside the container */
   &:hover {
-    background-color: ${(props) => (props.$activeLink ? "none" : "#f1f1f1")};
+    background-color: ${(props) => (props.$activeLink ? "none" : "#7317de")};
   }
 `;
 
@@ -87,7 +98,7 @@ export function NavBarDropdown(props) {
       onMouseOver={() => setOpen(true)}
       onMouseLeave={handleClose}
     >
-      {props.dropdownName}
+      {props.dropdownName} ▾
       <DropdownMenu onMouseLeave={handleToggle} open={open}>
         {props.dropdownItems.map((page) => (
           <DropdownItem
