81d8a9f419038a4c0bbf37521b4bcd85257bf834
Adon P M <apm1@systra.com>
2024-10-10 12:05:12 +0530

    Created all the page templates

---
+++ configs/evci/pages/actualArea.js
@@ -0,0 +1,53 @@
+import { selectors } from "../selectorDefinitions";
+
+export const actualArea = {
+  pageName: "Actual: Chargers/power by area",
+  url: "/@stbTag@/actual-area",
+  type: "MapLayout",
+  category: "@stbName@",
+  customLogoPath: "@logoPath@",
+  about: `
+  <p>Select an output to analyse. Each selection will show further options and fill the map panel with results.
+    To get further help, type or select a topic in the glossary box.</p>
+  <p>EV uptake gives the number of electric vehicles (EVs) that are expected across the region, 
+    split by powertrain type (battery electric or plug-in hybrid) and vehicle type (car, van, or heavy goods vehicle).</p>`,
+  config: {
+    layers: [
+      {
+        name: "Administrative Boundaries",
+        type: "tile",
+        source: "api",
+        path: "/api/vectortiles/zones/{zoneTypeId}/{z}/{x}/{y}", // matches the path in swagger.json
+        sourceLayer: "zones",
+        geometryType: "polygon",
+        visualisationName: "Actual Area",
+        isHoverable: true,
+        isStylable: true,
+        shouldHaveTooltipOnHover: true,
+        shouldHaveLabel: true,
+        labelZoomLevel: 12,
+        labelNulls: false,
+        hoverNulls: false,
+        hoverTipShouldIncludeMetadata: true,
+      }
+    ],
+    visualisations: [
+      {
+        name: "Actual Area",
+        type: "joinDataToMap",
+        joinLayer: "Administrative Boundaries",
+        style: "polygon-continuous",
+        joinField: "id",
+        valueField: "value",
+        dataSource: "api",
+        dataPath: "/api/evci/actual-area",
+      },
+    ],
+    metadataTables: [],
+    filters: [
+      { ...selectors.administrativeBoundary, visualisations: ['Actual Area'] },
+      { ...selectors.areaValueDisplay, visualisations: ['Actual Area'] },
+      { ...selectors.chargerSpeed, visualisations: ['Actual Area'] },
+    ],
+  },
+};
---
+++ configs/evci/pages/actualLocation.js
@@ -0,0 +1,51 @@
+import { selectors } from "../selectorDefinitions";
+
+export const actualLocation = {
+  pageName: "Actual: Chargers/power by location",
+  url: "/@stbTag@/actual-location",
+  type: "MapLayout",
+  category: "@stbName@",
+  customLogoPath: "@logoPath@",
+  about: `
+  <p>Select an output to analyse. Each selection will show further options and fill the map panel with results.
+    To get further help, type or select a topic in the glossary box.</p>
+  <p>EV uptake gives the number of electric vehicles (EVs) that are expected across the region, 
+    split by powertrain type (battery electric or plug-in hybrid) and vehicle type (car, van, or heavy goods vehicle).</p>`,
+  config: {
+    layers: [
+      {
+        name: "Administrative Boundaries",
+        type: "tile",
+        source: "api",
+        path: "/api/vectortiles/zones/{zoneTypeId}/{z}/{x}/{y}", // matches the path in swagger.json
+        sourceLayer: "zones",
+        geometryType: "polygon",
+        visualisationName: "Actual Location",
+        isHoverable: true,
+        isStylable: true,
+        shouldHaveTooltipOnHover: true,
+        shouldHaveLabel: true,
+        labelZoomLevel: 12,
+        labelNulls: false,
+        hoverNulls: false,
+        hoverTipShouldIncludeMetadata: true,
+      }
+    ],
+    visualisations: [
+      {
+        name: "Actual Location",
+        type: "joinDataToMap",
+        joinLayer: "Administrative Boundaries",
+        style: "polygon-continuous",
+        joinField: "id",
+        valueField: "value",
+        dataSource: "api",
+        dataPath: "/api/evci/actual-location",
+      },
+    ],
+    metadataTables: [],
+    filters: [
+      { ...selectors.chargerSpeed, visualisations: ['Actual Location'] },
+    ],
+  },
+};
