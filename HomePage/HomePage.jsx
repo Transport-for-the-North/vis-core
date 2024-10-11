62f573cf68a540a3165c7e9f055540522733508f
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-10-11 16:37:19 +0100

    Update HomePage styles and add Footer

--- HomePage/HomePage.jsx
+++ HomePage/HomePage.jsx
@@ -1,51 +1,53 @@
 import React, { useContext } from "react";
 import parse from "html-react-parser";
-
+import { Footer } from ".";
 import "./HomePage.styles.css";
-import { AppContext } from 'contexts';
+import { AppContext } from "contexts";
 
 /**
  * HomePage component represents the main landing page of the application.
  * It displays information retrieved from the AppContext such as title, introduction, background,
  * and contact details.
- * 
+ *
  * @component
  * @returns {JSX.Element} The JSX element representing the HomePage.
  */
 export const HomePage = () => {
   const appContext = useContext(AppContext);
-  
+
   return (
-    <div className="landing">
-      <div
-        className="header-landing"
-        //placeholder="Background Image for the header"
-        style={{
-           backgroundImage: `url(${appContext.backgroundImage})`
-        }}
-      >
-        <div className="container">
-          <h1 className="header-title">{appContext.title}</h1>
+    <>
+      <div className="landing">
+        <div
+          className="header-landing"
+          style={{
+            backgroundImage: `url(${appContext.backgroundImage})`,
+          }}
+        >
+          <div className="container">
+            <h1 className="header-title">{appContext.title}</h1>
+          </div>
+        </div>
+        {appContext.introduction !== "" && (
+          <section className="introduction">
+            <h2>About</h2>
+            <p className="container-intro">{parse(appContext.introduction)}</p>
+          </section>
+        )}
+        {appContext.background !== "" && (
+          <section className="background">
+            <h2>Background</h2>
+            <p className="container-bg">{parse(appContext.background)}</p>
+          </section>
+        )}
+        <div className="contacts">
+          <p>{appContext.contactText}</p>
+          <a className="contact" href={"mailto:" + appContext.contactEmail}>
+            {"Email: " + appContext.contactEmail}
+          </a>
         </div>
       </div>
-      {appContext.introduction !== "" && (
-        <section className="introduction">
-          <h2>About this tool</h2>
-          <p className="container-intro">{parse(appContext.introduction)}</p>
-        </section>
-      )}
-      {appContext.background !== "" && (
-        <section className="background">
-          <h2>Background of the app</h2>
-          <p className="container-bg">{parse(appContext.background)}</p>
-        </section>
-      )}
-      <div className="contacts">
-        <p>{appContext.contactText}</p>
-        <a className="contact" href={"mailto:" + appContext.contactEmail}>
-          {"Email: "+ appContext.contactEmail}
-        </a>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
