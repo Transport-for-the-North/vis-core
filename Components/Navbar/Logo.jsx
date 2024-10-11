4ce1f66d9aa79ddc54d66d9be9318021a61983ba
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-10-12 00:35:01 +0100

    Put logo in container so that the navbar elements don't resize between home and page

--- Components/Navbar/Logo.jsx
+++ Components/Navbar/Logo.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components"; // Ensure styled-components is imported
 
-const LogoImage = styled.img`
-  width: auto;
-  max-width: 192px;
-  max-height: 192px;
+const LogoContainer = styled.div`
+  width: 192px;
+  height: 192px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   @media only screen and (min-width: 766px) {
     padding: 1rem;
@@ -17,21 +19,27 @@ const LogoImage = styled.img`
   }
 `;
 
+const LogoImage = styled.img`
+  width: auto;
+  max-width: 100%;
+  max-height: 100%;
+`;
+
 /**
  * Renders a logo component.
  * @param {Object} props - The props for the Logo component.
  * @property {Function} props.onClick - The function to handle click events.
+ * @property {string} props.logoImage - The URL of the logo image.
  * @returns {JSX.Element} The rendered Logo component.
  */
 export function Logo(props) {
   const handleClick = () => {
     props.onClick();
   };
+
   return (
-    props.logoImage && <LogoImage
-      src={props.logoImage}
-      alt="Logo"
-      onClick={handleClick}
-    />
+    <LogoContainer onClick={handleClick}>
+      {props.logoImage && <LogoImage src={props.logoImage} alt="Logo" />}
+    </LogoContainer>
   );
 }
