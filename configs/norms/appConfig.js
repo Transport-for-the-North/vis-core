cefaaad07782eb44f5e78d9f0bc91d31fabe91df
Caitlin-Stokes <Caitlin.Stokes@transportforthenorth.com>
2024-10-17 15:38:09 +0100

    Update station catchment filters

--- configs/norms/appConfig.js
+++ configs/norms/appConfig.js
@@ -79,19 +79,19 @@ const scenarioCodeValues = {
     },
     {
       displayValue: "TBG 2042",
-      paramValue: "TBG_2042",
-    },
-    {
-      displayValue: "UCS 2042",
-      paramValue: "UCS_2042",
+      paramValue: "TBG_2042"
     },
     {
       displayValue: "TBH 2052",
-      paramValue: "TBH_2052",
+      paramValue: "TBH_2052"
+    },
+    {
+      displayValue: "UCS 2042",
+      paramValue: "UCS_2042"
     },
     {
       displayValue: "UCT 2052",
-      paramValue: "UCT_2052",
+      paramValue: "UCT_2052"
     },
   ],
 };
@@ -141,6 +141,46 @@ const userClassIdValues = {
       displayValue: "Other, all car availabilities",
       paramValue: "789",
     },
+    {
+      displayValue: "All",
+      paramValue: "0",
+    },
+    {
+      displayValue: "Business, car available from home",
+      paramValue: "1",
+    },
+    {
+      displayValue: "Business, car available to home",
+      paramValue: "2",
+    },
+    {
+      displayValue: "Business, car non available",
+      paramValue: "3",
+    },
+    {
+      displayValue: "Commuting, car available from home",
+      paramValue: "4",
+    },
+    {
+      displayValue: "Commuting, car available to home",
+      paramValue: "5",
+    },
+    {
+      displayValue: "Commuting, car non available",
+      paramValue: "6",
+    },
+    {
+      displayValue: "Other, car available from home",
+      paramValue: "7",
+    },
+    {
+      displayValue: "Other, car available to home",
+      paramValue: "8",
+    },
+    {
+      displayValue: "Other, car non available",
+      paramValue: "9",
+    }
   ]
 }
 
@@ -149,11 +189,11 @@ const originOrDestinationValues = {
   values: [
     {
       displayValue: "Origin",
-      paramValue: "origin",
+      paramValue: "1"
     },
     {
       displayValue: "Destination",
-      paramValue: "destination",
+      paramValue: "0",
     },
   ],
 }
@@ -1987,7 +2027,7 @@ export const appConfig = {
               displayColumn: "name",
               paramColumn: "id",
               sort: "ascending",
-              exclude: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+              exclude: [123, 456, 789]
             },
           },
           {
