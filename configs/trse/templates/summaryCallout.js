27ce662c1d2797c90184e2ef7de3995e79e5c94d
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-11-18 23:16:45 +0000

    Add formatting functions to replacePlaceholders

--- configs/trse/templates/summaryCallout.js
+++ configs/trse/templates/summaryCallout.js
@@ -2,10 +2,10 @@ import { replacePlaceholders } from "utils";
 
 const baseSummaryCallout = `<h2>{zone_name} TRSE Risk Summary</h2>
 <p>
-  In {zone_name}, <span class="highlight">{high_risk_pop}</span> residents <span class="highlight">({high_risk_perc}%)</span> live in neighbourhoods with a nationally high risk of TRSE. 
+  In {zone_name}, <span class="highlight">{formatNumber(high_risk_pop)}</span> residents <span class="highlight">({formatNumber(high_risk_perc)}%)</span> live in neighbourhoods with a nationally high risk of TRSE. 
 </p>
 <p>
-    This area ranks <span class="highlight">{high_risk_rank} out of {zone_count}</span> {authorityType} for the overall level of TRSE risk.
+    This area ranks <span class="highlight">{formatOrdinal(high_risk_rank)} out of {formatNumber(zone_count)}</span> {authorityType} for the overall level of TRSE risk.
 </p>`
 
 export const caSummaryCallout = replacePlaceholders(baseSummaryCallout, { authorityType: 'Combined Authorities' });
