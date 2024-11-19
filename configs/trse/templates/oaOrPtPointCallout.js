0cc23dbb241efc53db4de44bc42730f554f66ccb
Alistair Brereton-Halls <abrereton-halls@systra.com>
2024-11-19 14:52:21 +0000

    Move label above value in cards

--- configs/trse/templates/oaOrPtPointCallout.js
+++ configs/trse/templates/oaOrPtPointCallout.js
@@ -14,51 +14,59 @@ function generateDetailedCallout(region) {
     <h2>{feature_name}</h2>
     <div class="card-container">
       <div class="card">
-        <div class="value">{formatNumber(population)}</div>
         <div class="label">Population</div>
+        <div class="value">{formatNumber(population)}</div>
       </div>
       <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_trse_rank)}</div>
         <div class="label">TRSE Percentile</div>
+        <div class="value">{formatNumber(${rankPrefix}_trse_rank)}</div>
       </div>
     </div>
 
     <details>
-      <summary><strong>Vulnerability Percentile:</strong> {formatNumber(${rankPrefix}_vul_rank)}</summary>
+      <summary><strong>Vulnerability</strong></summary>
       <div class="card-container">
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_vul1_rank)}</div>
-        <div class="label">Disability and Caring Percentile</div>
+          <div class="label">Disability and Caring Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_vul1_rank)}</div>
         </div>
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_vul2_rank)}</div>
-        <div class="label">Childcare and Young People Percentile</div>
+          <div class="label">Childcare and Young People Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_vul2_rank)}</div>
         </div>
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_vul3_rank)}</div>
-        <div class="label">Low Income and Poverty Percentile</div>
+          <div class="label">Low Income and Poverty Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_vul3_rank)}</div>
+        </div>
+        <div class="card">
+          <div class="label">Overall Vulnerability Percentile</div>
+          <div class="value"> {formatNumber(${rankPrefix}_vul_rank)}</div>
         </div>
       </div>
     </details>
 
     <details>
-      <summary><strong>Access Percentile:</strong> {formatNumber(${rankPrefix}_acc_rank)}</summary>
+      <summary><strong>Access</strong></summary>
       <div class="card-container">
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_acc1_rank)}</div>
-        <div class="label">Work Access Percentile</div>
+          <div class="label">Work Access Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_acc1_rank)}</div>
         </div>
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_acc2_rank)}</div>
-        <div class="label">Education Access Percentile</div>
+          <div class="label">Education Access Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_acc2_rank)}</div>
         </div>
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_acc3_rank)}</div>
-        <div class="label">Health Access Percentile</div>
+          <div class="label">Health Access Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_acc3_rank)}</div>
         </div>
         <div class="card">
-        <div class="value">{formatNumber(${rankPrefix}_acc4_rank)}</div>
-        <div class="label">Amenities Access Percentile</div>
+          <div class="label">Amenities Access Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_acc4_rank)}</div>
+        </div>
+        <div class="card">
+          <div class="label">Overall Access Percentile</div>
+          <div class="value">{formatNumber(${rankPrefix}_acc_rank)}</div>
         </div>
       </div>
     </details>
