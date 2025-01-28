c4efef1af47484c7190a402336390b730588f5ef
Adon P M <apm1@systra.com>
2025-01-28 14:59:21 +0530

    Added "bsip-internal" config

---
+++ configs/bsip-internal/appConfig.js
@@ -0,0 +1,20 @@
+import { pages as bsipPages } from "../bsip/pages";
+import { appConfig as bsipAppConfig } from "configs/bsip/appConfig";
+
+
+export const appConfig = {
+    ...bsipAppConfig,
+    title: "Bus Analytics Tool - Internal",
+    appPages: [
+        ...Object.values(bsipPages).map(page => ({
+            ...page,
+            config: {
+                ...page.config,
+                visualisations: page.config.visualisations.map((visualisation) => ({
+                    ...visualisation,
+                    dataPath: visualisation.name === "Bus Accessibility" ? "/api/bsip/accessibility/staging" : visualisation.dataPath
+                }))
+            }
+        }))
+    ]
+};
