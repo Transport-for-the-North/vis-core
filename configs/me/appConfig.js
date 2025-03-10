59cfa64a1c8ae3e88f4306bf349ffdc80f860203
NicholasAJackson7 <139215348+NicholasAJackson7@users.noreply.github.com>
2025-03-10 14:44:02 +0000

    m&e feedback

--- configs/me/appConfig.js
+++ configs/me/appConfig.js
@@ -12,32 +12,29 @@ export const appConfig = {
     It integrates the targets from the Strategic Transport Plan (STP),
      published in April 2024 and data visualisations to help measure progress towards the ambitions of the STP. </p>
      
-     <p> The STP Monitoring and Evaluation Metrics Dashboard presents the 19 headline metrics that TfN 
-     uses to measure our progress towards achieving the three strategic ambitions set out in our STP.  
-     The overall approach to monitoring progress on delivering the STP is set out in the Monitoring and
-    Evaluation Strategy which complements this dashboard. The 19 headline metrics displayed in this dashboard are of the
-    highest strategic importance and define the vision of the STP, with most having associated medium-term and long-term
-    targets based on our evidence. The dashboard will be used annually to report progress against the headline metrics
-    and identify where further actions or policies may be necessary to support the STP trajectory.
-    These outcomes will inform TfN's annual Business Plans, which set out what TfN will do as an
-    organisation to support delivery of our collective vision. </p>
+     <p> The STP Monitoring and Evaluation Dashboard (M&E dashboard)  presents the headline metrics that TfN use to measure our progress towards achieving the three strategic ambitions set out in our STP.
+       The overall approach to monitoring progress on delivering the STP is set out in the Monitoring and Evaluation Strategy which complements the M&E dashboard. 
+       The headline metrics displayed in the M&E dashboard are of the highest strategic importance and define the vision of the STP, with most having associated medium-term and long-term targets based on our evidence. 
+       The dashboard will be used annually to report progress against the headline metrics and identify where further actions or policies may be necessary to support the STP trajectory. 
+       These outcomes will inform TfN's annual Business Plans, which set out what TfN will do as an organisation to support delivery of our collective vision.  </p>
     
-    <p>The targets set in the Strategic Transport Plan and displayed in the metrics dashboard are pan-Northern targets.
-    TfN recognises that local targets may vary, due to variations in place types across the North; Local Transport 
-    Authorities will progress towards their own specified targets at different paces. 
+    <p>The targets set in the Strategic Transport Plan and displayed in the M&E dashboard are pan-Northern targets. 
+    TfN recognises that local targets may vary, due to variations in place types across the North; 
+    Local Transport Authorities will progress towards their own specified targets at different paces.</p> 
      `,
-  background:"",
-  methodology:
-    `<p>The metrics dashboard provides time series data, where appropriate,  for each of the STP targets,  
-    including a baseline measurement for each. The data sources for the metrics are a mix of open-source data, 
-    data from TfN’s in-house modelling (our Analytical Framework), and commissioned data to measure progress.</p>`,
+  background:`
+  The M&E dashboard provides time series data, where appropriate,  for each of the STP targets,  including a baseline measurement for each. 
+  The data sources for the metrics are a mix of open-source data, data from TfN’s in-house modelling (our Analytical Framework), 
+  and commissioned data to measure progress.  
+  `,
+  methodology:"",
   legalText: termsOfUse,
   contactText: "Please contact research@transportforthenorth.com for any questions on this data tool.",
   contactEmail: "research@transportforthenorth.com",
   mapStyle: mapStyles.geoapifyPositron,
   logoImage: "img/tfn-logo-fullsize.png",
   logoPosition: 'left',
-  backgroundImage: "img/me/M&E_bg.jpg",
+  backgroundImage: "img/me/me_homepage.png",
   logoutButtonImage: "img/burgerIcon.png",
   logoutImage: "img/logout.png",
   authenticationRequired: false,
@@ -45,11 +42,12 @@ export const appConfig = {
     return bands.bands;
   },
   appPages: [
+    mePages.mePowerBi, // Include M&E-specific page
     ...Object.values(trsePages).map(page => ({
       ...page, // Spread the properties of the page
       category: "TRSE", // Set the category to "TRSE"
-    })), // Include TRSE pages
-    mePages.mePowerBi, // Include M&E-specific page
+    })) // Include TRSE pages
+    
   ],
   footer: {
     creditsText: "© Transport for the North 2024. All rights reserved.",
