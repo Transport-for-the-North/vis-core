b453b0087577edeae295b785001af1dfdebc559b
Adonpm <apm1@systra.com>
2025-03-25 12:52:48 +0530

    Updated 'Metrics dashboard' tab to read 'M&E dashboard'

--- configs/me/pages/me-power-bi.js
+++ configs/me/pages/me-power-bi.js
@@ -1,7 +1,7 @@
 import { termsOfUse } from "../termsOfUse";
 
 export const mePowerBi = {
-  pageName: "Metrics Dashboard",
+  pageName: "M&E dashboard",
   url: "/me-metrics",
   type: "IFrameEmbed",
   category: null,
