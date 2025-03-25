49562fc85b80283cb315f452e0a3e38855e54a0a
Alistair Brereton-Halls <abrereton-halls@systra.com>
2025-03-25 17:34:41 +0000

    severance: minor callout style change

--- configs/severance/templates/severanceCallout.js
+++ configs/severance/templates/severanceCallout.js
@@ -10,22 +10,21 @@
       <div class="value">{formatNumber(population_census_2021)}</div>
     </div>
     <div class="card">
-      <div class="label">Severance Risk Decile</div>
+      <div class="label">Severance index</div>
       <div class="value">{formatNumber(overall_decile_risk)}</div>
     </div>
     <div class="card">
-    <div class="label">Severance Risk String</div>
-    <div class="value">{formatNumber(overall_decile_string)}</div>
-    </div>
-    <div>
-      <h2>Severance index scores based on the presence of {barrier_name}</h2>
-      <p>
-        If this area had no <span class="highlight">{barrier_name}</span> infrastructure acting as a 
-        <span class="highlight">potential active travel barrier</span>, residents in this OA would have 
-        <span class="highlight">additional walkable access</span> to 
-        {formatNumber(perfect_reach_decile)} <span class="highlight">{destination_name} destinations</span> 
-        (current walkable access: {formatNumber(isochrone_reach_decile)}) 
-      </p>
+    <div class="label">Severance risk classification</div>
+    <div class="value">{overall_decile_string}</div>
     </div>
   </div>
+  <div>
+      <p>
+        If this area had no <span class="highlight">{barrier_name}</span> infrastructure acting as a 
+        potential active travel barrier, residents in this OA would have 
+        additional walkable access to 
+        <span class="highlight">{formatNumber(perfect_reach_decile)} {destination_name} destinations</span> 
+        (current walkable access: <span class="highlight">{formatNumber(isochrone_reach_decile)}</span>) 
+      </p>
+    </div>
 `;
