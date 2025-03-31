caa375077c8c6c4df23f12bfa80573a086a714d1
NicholasAJackson7 <139215348+NicholasAJackson7@users.noreply.github.com>
2025-03-31 12:27:34 +0000

    Revert "Merge pull request #234 from Transport-for-the-North/adding-sev-disclaimer"
    This reverts commit 40384b65bda6564fa9987a514f3533e1744cc8c4, reversing
    changes made to a269b04cd0f9ba426b75748a481f0a5d5ee4228c.

--- withWarning.jsx
+++ withWarning.jsx
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
 import { WarningMessage } from "Components";
-import { api } from "services";
 
 /**
- * Component to render a warning message.
- * @param {Object} props - The component props.
- * @param {string} props.message - The warning message to display.
- * @param {Function} props.onClose - Function to call when the warning is closed.
- * @returns {JSX.Element} The warning message component.
- */
-const Warning = ({ message, onClose }) => (
-  <WarningMessage>
-    <button className="close-button" onClick={onClose}>
-      &times;
-    </button>
-    <div className="icon-container">
-      <ExclamationCircleIcon className="warning-icon" />
-    </div>
-    <div className="text-container">
-      <div className="warning-header">
-        <div className="warning-title">Warning</div>
-      </div>
-      <div className="warning-content">
-        {message}
-      </div>
-    </div>
-  </WarningMessage>
-);
-
-/**
- * Higher-Order Component that adds a warning message to the wrapped component.
- * It can handle both static and dynamic warnings.
+ * Higher-Order Component that adds a warning message to the wrapped component if the pageConfig contains a warning property.
  * @param {React.ComponentType} WrappedComponent - The component to wrap.
  * @returns {React.ComponentType} The wrapped component with a warning message if applicable.
  */
@@ -39,48 +11,30 @@ export const withWarning = (WrappedComponent) => {
   return (props) => {
     const { pageConfig } = props;
     const [isVisible, setIsVisible] = useState(true);
-    const [dynamicMessage, setDynamicMessage] = useState(null);
 
     const handleClose = () => {
       setIsVisible(false);
     };
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const dynamicWarningConfig = pageConfig.config?.additionalFeatures?.dynamicWarning;
-        if (dynamicWarningConfig && dynamicWarningConfig.url && dynamicWarningConfig.template) {
-          const controller = new AbortController();
-          try {
-            const response = await api.baseService.get(dynamicWarningConfig.url, { signal: controller.signal });
-            console.log("Response Body:", response);
-            const data = response.data;
-            const message = response.message;
-            let dynamicMessage = dynamicWarningConfig.template.replace(/{(\w+)}/g, (_, key) => data || '');
-            if (message) {
-              dynamicMessage += 
-                '\n</span><span class="si">${</span><span class="nx">message</span><span class="si">}</span><span class="sb">';
-            }
-            setDynamicMessage(dynamicMessage);
-          } catch (error) {
-            if (error.name !== "AbortError") {
-              console.error("Error fetching dynamic warning data:", error);
-            }
-          }
-          return () => {
-            controller.abort();
-          };
-        }
-      };
-      fetchData();
-    }, [pageConfig]);
-
-    const staticWarning = pageConfig.config?.additionalFeatures?.warning;
-    const warningMessage = dynamicMessage || staticWarning;
-
     return (
       <>
-        {warningMessage && isVisible && (
-          <Warning message={warningMessage} onClose={handleClose} />
+        {pageConfig.config?.additionalFeatures?.warning && isVisible && (
+          <WarningMessage>
+            <button className="close-button" onClick={handleClose}>
+              &times;
+            </button>
+            <div className="icon-container">
+              <ExclamationCircleIcon className="warning-icon" />
+            </div>
+            <div className="text-container">
+              <div className="warning-header">
+                <div className="warning-title">Warning</div>
+              </div>
+              <div className="warning-content">
+                {pageConfig.config?.additionalFeatures?.warning}
+              </div>
+            </div>
+          </WarningMessage>
         )}
         <WrappedComponent {...props} />
       </>
