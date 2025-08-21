9714cd8c4b23552829c27468d5dd17b1d2859523
VAN ELSUVE Pierre <pvanelsuve@systra.info>
2025-08-21 14:55:37 +0100

    Added unit tests, MessageBox and NotFoundPage

---
+++ __tests__/MessageBox/MessageBox.test.jsx
@@ -0,0 +1,39 @@
+import { InfoBox } from "../../MessageBox/MessageBox";
+import { WarningBox } from "../../MessageBox/MessageBox";
+import { ErrorBox } from "../../MessageBox/MessageBox";
+
+import { render, screen } from "@testing-library/react";
+
+jest.mock("@heroicons/react/24/solid",()  => ({
+    ...jest.requireActual("@heroicons/react/24/solid"),
+    InformationCircleIcon: () => <div>IconInfo</div>,
+    XCircleIcon: () => <div>IconError</div>,
+    ExclamationTriangleIcon: () => <div>IconWarning</div>,
+}));
+
+describe("MessageBox test component", () => {
+  it("Type warning", () => {
+    render(<WarningBox text="This is a warning message" />);
+    const el = screen.getByText("This is a warning message");
+    expect(el).toBeInTheDocument();
+    expect(el).toHaveStyle("background-color: rgba(255, 255, 153, 0.9);");
+    expect(el).toHaveStyle("color: rgb(102, 60, 0);");
+    expect(screen.getByText("IconWarning")).toBeInTheDocument();
+  });
+  it("Type error", () => {
+    render(<ErrorBox text="This is a warning message" />);
+    const el = screen.getByText("This is a warning message");
+    expect(el).toBeInTheDocument();
+    expect(el).toHaveStyle("background-color: #ffebee;");
+    expect(el).toHaveStyle("color: #d32f2f;");
+    expect(screen.getByText("IconError")).toBeInTheDocument();
+  });
+  it("Type info", () => {
+    render(<InfoBox text="This is a warning message" />);
+    const el = screen.getByText("This is a warning message");
+    expect(el).toBeInTheDocument();
+    expect(el).toHaveStyle("background-color: rgba(0, 222, 198, 0.9);");
+    expect(el).toHaveStyle("color: rgb(13, 15, 61);");
+    expect(screen.getByText("IconInfo")).toBeInTheDocument();
+  });
+});
