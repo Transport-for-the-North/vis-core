8542904a14237f1e174c728a2831b5a3a4d155e5
NicholasAJackson7 <139215348+NicholasAJackson7@users.noreply.github.com>
2025-09-15 21:39:40 +0100

    Update index.js

--- packages/vis-core/dist/index.js
+++ packages/vis-core/dist/index.js
@@ -1,5 +1,5 @@
 import { jsx as r } from "react/jsx-runtime";
-const e = () => "Hello from vis-core";
+const e = () => "Hello from vis-core!";
 function n({ children: o }) {
   return /* @__PURE__ */ r("button", { style: { padding: 8, borderRadius: 6 }, children: o });
 }
