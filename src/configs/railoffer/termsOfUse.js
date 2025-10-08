804d933006faeae9b944a65fb13b5957fc7dc18d
NicholasAJackson7 <139215348+NicholasAJackson7@users.noreply.github.com>
2025-10-08 17:34:10 +0100

    moving src with history into package, renamed old one src2

--- src/configs/railoffer/termsOfUse.js
+++
@@ -1,5 +0,0 @@
-export const termsOfUse = `
-<p>
-For our terms of use, please see the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" target="_blank">Open Government Licence</a>. Use of the Rail Information Portal Visualiser Tool also indicates your acceptance of this <a href="https://tfnshare.blob.core.windows.net/visdocs/Licensing%20of%20TfN%20Tools-Rail_Offer.pdf?sv=2023-01-03&st=2025-07-17T10%3A48%3A09Z&se=2027-04-02T10%3A48%3A00Z&sr=b&sp=r&sig=HTApkE4OPibNbFDdnmSRvyoWiRtnPptT0Qo9ZXPE56c%3D" target="_blank">Disclaimer and Appropriate Use Statement</a>
-</p>
-`// Placeholder currently.
