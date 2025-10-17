df8c0ff1ca9cd47855a14f8c5b087698ebb6cd97
Alistair Brereton-Halls <abrereton-halls@systra.com>
2025-10-17 09:58:54 +0100

    Add filters to example mapImage page

--- configs/avp/pages/mapImage.js
+++ configs/avp/pages/mapImage.js
@@ -32,13 +32,17 @@ export const mapImage = {
             {
                 name: "Detailed Information",
                 type: "calloutCard",
-                cardName: "Local Authority Summary",
+                cardName: "",
                 dataSource: "api",
                 dataPath: "/api/avp/pca/locations/{id}",
-                htmlFragment: '<p>{name}-{category}-{percent:id}</p>'
+                htmlFragment: '<p>{programme_id}-{label}-{location_id}-{text_with_placeholders}</p>'
             }
         ],
         metadataTables: [
+            {
+                name: "v_vis_avp_programmes_run_info",
+                path: "/api/getgenericdataset?dataset_id=views_vis.v_vis_avp_programmes_run_info"
+            }
         ],
         filters: [
             {
@@ -50,6 +54,192 @@ export const mapImage = {
                 type: "map",
                 layer: "PCA Locations",
                 field: "id",
+            },
+            {
+                filterName: "Catalogue version",
+                paramName: null,
+                target: "api",
+                actions: [],
+                visualisations: ["Detailed Information"],
+                info:"",
+                type: "dropdown",
+                shouldBeBlankOnInit: false,
+                shouldFilterOnValidation: false,
+                shouldFilterOthers: false,
+                shouldBeValidated: false,
+                isClearable: false,
+                multiSelect: false,
+                values: {
+                    source: "metadataTable",
+                    metadataTableName: "v_vis_avp_programmes_run_info",
+                    displayColumn: "nortms_catalog_version",
+                    paramColumn: "nortms_catalog_version",
+                    sort: "ascending",
+                    where: [
+                        {
+                            column: 'has_pca',
+                            values: false,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'programme_id',
+                            values: 2,
+                            operator: 'equals'
+                        },
+                    ]
+                },
+            },
+            {
+                filterName: "Demand scenario",
+                paramName: null,
+                target: "api",
+                actions: [
+                    { action: "UPDATE_QUERY_PARAMS" },
+                ],
+                visualisations: ["Detailed Information"],
+                info:"",
+                type: "dropdown",
+                shouldBeBlankOnInit: false,
+                shouldFilterOnValidation: false,
+                shouldFilterOthers: false,
+                shouldBeValidated: false,
+                isClearable: false,
+                multiSelect: false,
+                values: {
+                    source: "metadataTable",
+                    metadataTableName: "v_vis_avp_programmes_run_info",
+                    displayColumn: "demand_scenario",
+                    paramColumn: "demand_scenario",
+                    sort: "ascending",
+                    where: [
+                        {
+                            column: 'has_pca',
+                            values: false,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'programme_id',
+                            values: 2,
+                            operator: 'equals'
+                        },
+                    ]
+                },
+            },
+            {
+                filterName: "DoMin Network Scenario",
+                paramName: null,
+                target: "api",
+                actions: [
+                    { action: "UPDATE_QUERY_PARAMS" },
+                ],
+                visualisations: ["Detailed Information"],
+                info:"",
+                type: "dropdown",
+                shouldBeBlankOnInit: false,
+                shouldFilterOnValidation: false,
+                shouldFilterOthers: false,
+                shouldBeValidated: false,
+                isClearable: false,
+                multiSelect: false,
+                values: {
+                    source: "metadataTable",
+                    metadataTableName: "v_vis_avp_programmes_run_info",
+                    displayColumn: "network_scenario",
+                    paramColumn: "network_scenario",
+                    sort: "ascending",
+                    where: [
+                        {
+                            column: 'has_pca',
+                            values: false,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'programme_id',
+                            values: 2,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'network_type',
+                            values: 'dm',
+                            operator: 'equals'
+                        },
+                    ]
+                },
+            },
+            {
+                filterName: "DoSom Network Scenario",
+                paramName: null,
+                target: "api",
+                actions: [
+                    { action: "UPDATE_QUERY_PARAMS" },
+                ],
+                visualisations: ["Detailed Information"],
+                info:"",
+                type: "dropdown",
+                shouldBeBlankOnInit: false,
+                shouldFilterOnValidation: false,
+                shouldFilterOthers: false,
+                shouldBeValidated: false,
+                isClearable: false,
+                multiSelect: false,
+                values: {
+                    source: "metadataTable",
+                    metadataTableName: "v_vis_avp_programmes_run_info",
+                    displayColumn: "network_scenario",
+                    paramColumn: "network_scenario",
+                    sort: "ascending",
+                    where: [
+                        {
+                            column: 'has_pca',
+                            values: false,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'programme_id',
+                            values: 2,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'network_type',
+                            values: 'ds',
+                            operator: 'equals'
+                        },
+                    ]
+                },
+            },
+            {
+                filterName: "Year",
+                paramName: null,
+                target: "api",
+                actions: [],
+                visualisations: ["Detailed Information"],
+                info:"",
+                type: "dropdown",
+                shouldBeBlankOnInit: false,
+                shouldFilterOnValidation: true,
+                shouldFilterOthers: false,
+                shouldBeValidated: false,
+                isClearable: false,
+                multiSelect: false,
+                values: {
+                    source: "metadataTable",
+                    metadataTableName: "v_vis_avp_programmes_run_info",
+                    displayColumn: "year",
+                    paramColumn: "year",
+                    sort: "ascending",
+                    where: [
+                        {
+                            column: 'has_pca',
+                            values: false,
+                            operator: 'equals'
+                        },
+                        {
+                            column: 'programme_id',
+                            values: 2,
+                            operator: 'equals'
+                        },
+                    ]
+                },
             },
         ],
         additionalFeatures: {
