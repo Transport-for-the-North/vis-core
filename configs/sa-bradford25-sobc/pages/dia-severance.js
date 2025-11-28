604dbbf5d2672c5896a59c0d43fa9645c4b81f82
Alistair Brereton-Halls <abrereton-halls@systra.com>
2025-11-28 03:18:22 +0000

    Modify dia severance

--- configs/sa-bradford25-sobc/pages/dia-severance.js
+++ configs/sa-bradford25-sobc/pages/dia-severance.js
@@ -1,6 +1,8 @@
+import { layers } from "../layers";
+
 export const severance = {
   pageName: "Severance",
-  url: "/severance",
+  url: "/dia-severance",
   about: `<p>This is customisable 'about' text. Use it to describe what's being visualised and how the user can interact with the data. If required, add more useful context about the data.</p>`,
   type: "MapLayout",
   category: "DIA",
@@ -8,13 +10,15 @@ export const severance = {
   termsOfUse: "bar",
   config: {
     layers: [
+      layers.avpNetworkLineGeometryById,
       {
-        name: "Zones",
+        uniqueId: "DIALevelCrossing",
+        name: "Level Crossings",
         type: "tile",
         source: "api",
-        path: "api/vectortiles/{dia_level_crossing_geometry}/{z}/{x}/{y}",
-        sourceLayer: "zones",
-        geometryType: "polygon",
+        path: "/api/vectortiles/dia_level_crossing_accident_geometry/{z}/{x}/{y}?network_id={networkId}&programme_id={programmeId}",
+        sourceLayer: "geometry",
+        geometryType: "point",
         visualisationName: "Map-based totals",
         isHoverable: true,
         isStylable: true,
@@ -26,43 +30,78 @@ export const severance = {
         hoverTipShouldIncludeMetadata: false,
         invertedColorScheme: false,
         outlineOnPolygonSelect: true,
-      },
+        customTooltip: {
+          url: "/api/dia/severance/callout-data?severanceId={id}&networkId={networkId}&programmeId={programmeId}",
+          htmlTemplate: `
+            <div class="popup-content">
+              <div class="metadata-item">
+                <span class="metadata-key">Crossing Name:</span>
+                <span class="metadata-value">{crossing_name}</span>
+              </div>
+              <div class="metadata-item">
+                <span class="metadata-key">Crossing Type:</span>
+                <span class="metadata-value">{crossing_type}</span>
+              </div>
+              <div class="metadata-item">
+                <span class="metadata-key">{value}:</span>
+                <span class="metadata-value">{value}</span>
+              </div>
+              <div class="metadata-item">
+                <span class="metadata-key">Modification:</span>
+                <span class="metadata-value">{modification}</span>
+              </div>
+            </div>
+        `
+        }
+      }
     ],
     visualisations: [
       {
         name: "Map-based totals",
         type: "joinDataToMap",
-        joinLayer: "Zones",
-        style: "polygon-continuous",
+        joinLayer: "Level Crossings",
+        style: "circle-categorical",
+        dynamicStyling: true,
         joinField: "id",
         valueField: "value",
         dataSource: "api",
-        dataPath: "/api/dia/accidents/zonal-data",
+        dataPath: "/api/dia/severance/zonal-data",
       },
     ],
     metadataTables: [
       {
-        name: "dia_severance_outputs",
-        path: "/api/getgenericdataset?dataset_id=avp_data.dia_severance_outputs",
+        name: "avp_networks",
+        path: "/api/getgenericdataset?dataset_id=avp_data.avp_networks",
       },
     ],
     filters: [
       // networkId
       {
-        filterName: "networkId",
+        filterName: "Network Scenario",
         paramName: "networkId",
         target: "api",
-        actions: [{ action: "UPDATE_QUERY_PARAMS" }],
+        actions: [
+          {
+            action: "UPDATE_PARAMETERISED_LAYER",
+            payload: { targetLayer: "Level Crossings" },
+          },
+          {
+            action: "UPDATE_PARAMETERISED_LAYER",
+            payload: { targetLayer: "Network" },
+          },
+          { action: "UPDATE_QUERY_PARAMS" },
+        ],
         visualisations: ["Map-based totals"],
-        layer: "Zones",
         type: "dropdown",
         values: {
           source: "metadataTable",
-          metadataTableName: "dia_severance_outputs",
-          displayColumn: "network_id",
-          paramColumn: "network_id",
-          sort: "ascending",
-          where: [{ column: "network_id", operator: "notNull" }],
+          metadataTableName: "avp_networks",
+          displayColumn: "network",
+          paramColumn: "id",
+          sort: "descending",
+          where: [{ column: "id", operator: "notNull" },
+            { column: "programme_id", operator: "equals", values: 2 }
+          ],
         },
       },
       // programmeId
@@ -70,17 +109,28 @@ export const severance = {
         filterName: "programmeId",
         paramName: "programmeId",
         target: "api",
-        actions: [{ action: "UPDATE_QUERY_PARAMS" }],
+        actions: [
+          {
+            action: "UPDATE_PARAMETERISED_LAYER",
+            payload: { targetLayer: "Level Crossings" },
+          },
+          {
+            action: "UPDATE_PARAMETERISED_LAYER",
+            payload: { targetLayer: "Network" },
+          },
+          { action: "UPDATE_QUERY_PARAMS" },
+        ],
         visualisations: ["Map-based totals"],
-        layer: "Zones",
         type: "fixed",
+        forceRequired: true,
         values: {
-          source: "metadataTable",
-          metadataTableName: "dia_severance_outputs",
-          displayColumn: "programme_id",
-          paramColumn: "programme_id",
-          sort: "ascending",
-          where: [{ column: "programme_id", operator: "notNull" }],
+          source: "local",
+          values: [
+            {
+              displayValue: "2",
+              paramValue: 2, // Put their true value
+            },
+          ],
         },
       },
     ],
