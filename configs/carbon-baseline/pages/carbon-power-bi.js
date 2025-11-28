6687e9b14fdb0bcd5701e53ef48413432f97a27b
Caitlin-Stokes <144812902+Caitlin-Stokes@users.noreply.github.com>
2025-11-28 15:14:24 +0000

    carbon-vis-first-commit

---
+++ configs/carbon-baseline/pages/carbon-power-bi.js
@@ -0,0 +1,14 @@
+import { termsOfUse } from "../termsOfUse";
+
+export const carbonPowerBi = {
+    pageName: "Carbon Baseline Dashboard",
+    url: "/carbon-dashboard",
+    type: "IFrameEmbed",
+    category: null,
+    legalText: termsOfUse,
+    termsOfUse: termsOfUse,
+    about: "",
+    config: {
+        url: "https://app.powerbi.com/reportEmbed?reportId=f460017f-b8eb-40b6-b69c-4ffa92149ea1&autoAuth=true&ctid=d58a9e1d-4dca-4595-a9e8-7934a15f3419"
+    }
+}
