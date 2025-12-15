49b1b3a5b626266a049e0e8d1a7a5dd6d509fcbb
Mangaiy <mangai.ramakrishnan@transportforthenorth.com>
2025-12-15 13:40:20 +0000

    hocs folder migrated to vis core

--- packages/vis-core/src/hocsNew/composeHocs.js
+++
@@ -1,8 +0,0 @@
-/**
- * Utility function to compose multiple HOCs.
- * @param  {...Function} hocs - The HOCs to compose.
- * @returns {Function} A function that applies all the HOCs to a component.
- */
-export const composeHOCs = (...hocs) => (Component) => {
-    return hocs.reduce((WrappedComponent, hoc) => hoc(WrappedComponent), Component);
-  };
