120ddf1ba0a96b79ab2c3bd7b11abab81a2841ba
NicholasAJackson7 <139215348+NicholasAJackson7@users.noreply.github.com>
2026-01-20 15:12:20 +0000

    feat: Add coordinate preview map and BNG/WGS84 conversion
    Introduces a new CoordinatePreviewMap component for interactive coordinate selection and preview using maplibre-gl. Adds coordinate conversion utilities for British National Grid (BNG) and WGS84 using proj4, and updates DynamicForm and PageSwitch to support conditional field visibility, checkbox/date fields, and map preview integration.

--- packages/vis-core/vite.config.ts
+++ packages/vis-core/vite.config.ts
@@ -37,11 +37,10 @@ export default defineConfig(({ command }) => ({
         `${entryName}.${format === 'es' ? 'js' : 'cjs'}`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime", "react-router-dom", "@mapbox/mapbox-gl-draw", 
-        "mapbox-gl", "@turf/turf", "chroma-js", "polished", "js-cookie", "@auth0/auth0-react", "lodash.debounce",
+      external: ["react", "react-dom", "react/jsx-runtime", "react-router-dom", "@mapbox/mapbox-gl-draw",
+        "mapbox-gl", "maplibre-gl", "@turf/turf", "chroma-js", "polished", "js-cookie", "@auth0/auth0-react", "lodash.debounce",
         "@heroicons/react", "leaflet", "lz-string", "jwt-decode", "html-react-parser", "dompurify", "colorbrewer",
-        "@ookla/mapbox-gl-draw-rectangle", "@mapcomponents/react-maplibre", "styled-components", "recharts",
-        "react-select"
+        "@ookla/mapbox-gl-draw-rectangle", "@mapcomponents/react-maplibre", "styled-components", "recharts", "react-select", "proj4"
       ],
       output: {
         globals: { react: "React", "react-dom": "ReactDOM" },
